b2bd56a7c84e204d11c80da133a2d1cf8bc8d536
Corbalán Calvo Nicolás <corn@elcaNet.local>
2021-12-07 13:46:23 +0100

    adding filters

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Filters from './components/Filters/Filters';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center poppins my-4">Rick & Morty <span className="text-primary">Wiki</span></h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-3"></div>
+        </div>
+      </div>
     </div>
   );
 }
