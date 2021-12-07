659412182bfc59e84add9e316cebac99f50e9a6b
Corbalán Calvo Nicolás <corn@elcaNet.local>
2021-12-07 15:13:59 +0100

    fetching data with IIFE function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 
+import React, { useState, useEffect } from 'react'
 import './App.css';
+import Cards from './components/Cards/Cards';
 import Filters from './components/Filters/Filters';
 
 function App() {
+
+  const [pageNumer, setPageNumber] = useState(1);
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumer}`;
+
+  useEffect(() => {
+    (async function(){
+      let data = await fetch(api).then((res) => res.json());
+      console.log(data.results);
+    })()
+  }, [api])
+
   return (
     <div className="App">
       <h1 className="text-center poppins my-4">Rick & Morty <span className="text-primary">Wiki</span></h1>
@@ -11,7 +24,13 @@ function App() {
           <div className="col-3">
             <Filters />
           </div>
-          <div className="col-3"></div>
+          <div className="col-8">
+            <div className="row">
+              <Cards />
+              <Cards />
+              <Cards />
+            </div>
+          </div>
         </div>
       </div>
     </div>
