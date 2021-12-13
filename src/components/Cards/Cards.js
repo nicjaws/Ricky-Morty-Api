d3d02504d9aa90dd2a3bee6c7074f337931206cb
Corbalán Calvo Nicolás <corn@elcaNet.local>
2021-12-13 16:34:13 +0100

    card style

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import styles from './Cards.module.scss';
 
 const Cards = ({results}) => {
-    const display;
+    let display;
     console.log(results)
 
     if (results) {
        display = results.map((item) => {
-           const {id} = item
-            return(<div key={id} className="col-4">asldkjfslkdjl</div>)
+           const {id, name, image, location, status} = item
+            return(
+                <div key={id} className="col-4 mb-4 position-relative">
+                    <div className={styles.cards}>
+                        <img src={image} alt="" className={`${styles.img} img-fluid`} />
+                        <div style={{padding: '10px'}}className="content">
+                            <div className="fs-4 fw-bold mb-4">{name}</div> 
+                            <div className="">
+                                <div className="fs-6">Last location</div>
+                                <div className="fs-5">{location.name}</div>
+                            </div>
+                        </div>
+                    </div>
+                    {(() => {
+                        if(status === 'Dead') {
+                           return <div className={`${styles.badge} position-absolute badge bg-danger`}>{status}</div>
+                        } else if(status === 'Alive') {
+                           return <div className={`${styles.badge} position-absolute badge bg-success`}>{status}</div>
+                        } else {
+                           return  <div className={`${styles.badge} position-absolute badge bg-secondary`}>{status}</div>
+                        }
+                    }
+                    )()
+                    }
+                    
+                </div>
+            );
        });
     } else {
        display =  "No characters found";
